4d6550944e5dbb9831d00d3f7dcde82f4b7d3ac8
readywater <alb@andrewlb.com>
2016-11-17 17:11:58 -0500

    Map loads in

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/examples.map-zr0njcqy/{z}/{x}/{y}.png', {
-        attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
-});
+var map = L.map('map').setView([51.505, -0.09], 13);
 
+L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-var map = L.map('map')
-    .addLayer(mapboxTiles)
-    .setView([40.72332345541449, -73.99], 10);
-    
+L.marker([51.5, -0.09]).addTo(map)
+    // .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    // .openPopup();
