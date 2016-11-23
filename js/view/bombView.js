ba714c24b905ee5ecfdf6a196acc7b932c78c531
readywater <alb@andrewlb.com>
2016-11-23 13:57:57 -0500

    Marker update animation

--- js/view/bombView.js
+++ js/view/bombView.js
@@ -6,12 +6,14 @@ muarg.Views = muarg.Views || {};
     muarg.Views.bombView = Backbone.View.extend({
         parent: {},
         model: {},
+        marker: {},
         events: {
         },
         initialize: function(option) {
         	var _this = this
             this.parent = option.parent
             this.render();
+            this.listenTo(this.parent.collection, 'change', this.changeColor);
         },
 
         render: function() {
@@ -19,12 +21,18 @@ muarg.Views = muarg.Views || {};
             var coord = [this.model.get("geometry_coordinates_1"),this.model.get("geometry_coordinates_0")]
             if(coord[0] == null || coord[1] == null) return
             // L.marker(coord).addTo(_this.parent.map)
-            L.circleMarker(coord, {
-                fillColor: "#708598",
+            this.marker = L.circleMarker(coord, {
+                fillColor: this.parent.collection.date()[1] == this.model.get("properties_date") ? 'red' : "#708598",
                 color: '#537898',
                 weight: 1,
                 fillOpacity: 0.6
-            }).addTo(_this.parent.map)
+            })
+            console.log(this.marker)
+            this.marker.addTo(_this.parent.map);
+        },
+
+        changeColor: function() {
+            this.marker.setStyle({'fillColor': this.parent.collection.date()[1] == this.model.get("properties_date") ? 'red' : "#708598"})
         },
 
         renderError: function() {
